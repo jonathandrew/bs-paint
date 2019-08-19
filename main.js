065a994c3c2a927e2d40d7223f9057f8f0c0f6b0
Jonathan Drew <jonathan.drew@codeimmersives.com>
2019-08-19 10:38:05 -0400

    final product

---
+++ main.js
@@ -0,0 +1,47 @@
+const pallete = document.querySelector(".pallete")
+pallete.addEventListener("click")
+
+const canvas = document.querySelector(".canvas")
+canvas.addEventListener("click")
+
+const brush = document.querySelector(".brush")
+brush.addEventListener("click")
+
+function color1(){
+const color1 = document.querySelector(".color1")
+}
+
+function color2(){
+const color2 = document.querySelector(".color2")
+}
+
+function color3(){
+const color3 = document.querySelector(".color3")
+
+}
+
+function color4(){
+const color4 = document.querySelector(".color4")
+}
+
+function color5(){
+const color5 = document.querySelector(".color5")
+}
+
+
+const objectOfColor = (
+
+    white;color1
+    blue;color2
+    green;color3
+    yellow;color4
+    purple;color5
+    
+
+)
+
+
+function changingColor(objectOfColor){
+    forEach(objectOfColor, color)
+    .onclick(brush)
+}
